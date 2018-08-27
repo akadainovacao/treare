cf53ea7918ad8e5dec9952afa5938099d1288bdb
Vincenzo G. Neto <vincenzo@redfox.tech>
2018-08-26 21:03:54 -0300

    versão sem materialize

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,3 +2,17 @@
 // window.innerHeight
 // screen.width
 // screen.height
+
+var sidemenu = document.getElementById('sidemenu');
+var close = document.getElementsByClassName('material-icons');
+sidemenu.addEventListener('click', openMenu);
+
+function openMenu() {
+  var menumobile = document.getElementsByClassName('menu-mobile');
+  menumobile[0].style.display = "block";
+  close[0].innerHTML = 'close'
+}
+
+if(close[0].innerHTML == 'close') {
+  console.log('ashusahds')
+}
